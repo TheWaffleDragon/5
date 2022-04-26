5e5d2eca1cdaaad8b6f44ba7c918b6041a464713
KB <baczewska.katarzyna@gmail.com>
2022-04-26 13:00:06 +0200

    nowe zadanie

--- Biblioteka.py
+++ Biblioteka.py
@@ -1,7 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 26 12:42:20 2022
+class Movie:
+    def __init__(self, title, year, genre, times_seen):
+        self.title = title
+        self.year = year
+        self.genre = genre
+        self.times_seen = times_seen
+        
 
-@author: Libero
-"""
-
+class TvShow(Movie):
+    def __init__(self, epi, season, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.epi = epi
+        self.season = season
+        
