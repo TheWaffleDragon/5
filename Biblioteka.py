4485a303109709664217cb761e7a7142e035e946
KB <baczewska.katarzyna@gmail.com>
2022-06-29 20:14:17 +0200

    dodatkowe zadania

--- Biblioteka.py
+++ Biblioteka.py
@@ -38,3 +38,95 @@ watch.append(tv_2)
 
 print(watch[0])
 print(watch[2])
+
+#%%
+#Dodtkowe zadanie
+'''
+Napisz funkcje get_movies oraz get_series, które będą filtrować listę 
+i zwracać odpowiednio tylko filmy oraz tylko seriale. Posortuj listę wynikową alfabetycznie.
+'''
+
+def get_movies(watch_list):
+    movies = []
+    for i in watch_list:
+        if type(i)==Movie:
+            movies.append(i)
+    return movies
+
+movies_from_watch = get_movies(watch)    
+
+def get_tv_shows(watch_list):
+    tv_show = []
+    for j in watch_list:
+        if type(j)==TvShow:
+            tv_show.append(j)
+    return tv_show 
+
+tv_show_from_watch = get_tv_shows(watch)    
+
+
+ #%%   
+'''
+Napisz funkcję search, która wyszukuje film lub serial po jego tytule.
+'''
+
+def search(watch_list,t):
+    serched_title = []
+    for i in watch_list:
+        if i.title == t:
+            serched_title.append(i)
+    return serched_title
+    
+title_from_list = search(watch, 'tv_1')
+#%%
+
+'''
+Napisz funkcję generate_views, która losowo wybiera element z biblioteki, 
+a następnie dodaje mu losową (z zakresu od 1 do 100) ilość odtworzeń.
+'''
+import random as rnd
+
+def generate_views(watch_list):
+    x = rnd.randint(0, len(watch_list)-1)
+    y = rnd.randint(1,100)
+    watch_list[x].times_seen = y
+    
+    
+generate_views(watch)
+
+
+
+
+
+#%%
+'''
+Napisz funkcję, która uruchomi generate_views 10 razy.
+'''
+def gen_10(watch_list):
+    for i in range(10):
+        generate_views(watch_list)
+
+
+gen_10(watch)
+
+
+
+
+#%%
+'''
+Napisz funkcję top_titles(), która zwróci wybraną ilość najpopularniejszych 
+tytułów z biblioteki. Dla chętnych: dodaj do funkcji parametr content_type, 
+którym wybierzesz czy mają zostać pokazane filmy, czy seriale.
+'''
+
+def sortby(watch_list):
+    return watch_list.times_seen    
+
+def top_titles(watch_list,n):
+    s = watch_list.sort(reverse=True, key = sortby)
+    for i in range(n):
+        print (watch_list[i])
+    
+
+top_titles(watch, 2)
+
